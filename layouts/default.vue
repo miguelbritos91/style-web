61be9f8a2010325a36f80e008b9e87531abcaa10
Miguel Britos <miguelbritos91@gmail.com>
2021-03-11 11:57:28 -0300

    Init project style-web 2021

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      fixed: false
+    }
+  }
+}
+</script>
